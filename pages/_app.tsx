1d1001b3920beff146da2dc337178875e1d65b44
kosu <kosumon27@gmail.com>
2022-12-02 19:02:17 +0900

    ✨ map外観

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable import/no-default-export */
+/* eslint-disable react/destructuring-assignment */
 import '../styles/globals.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
+export default App
