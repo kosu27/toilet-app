783c01b963d3a662d643b318a8c68a79cab60d7a
kosu <kosumon27@gmail.com>
2022-12-03 13:57:33 +0900

    🔧 vsCode設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: "jit",
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/typography"),
-        require("@tailwindcss/line-clamp"),
-    ],
-  };
+  mode: 'jit',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
+}
