2402b089c639cccec1eb987ca7f8eea564cc4152
kosu <kosumon27@gmail.com>
2022-12-05 23:53:54 +0900

    ✨ mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,22 @@
 import '../styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
+import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'light',
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
 
 export default App
