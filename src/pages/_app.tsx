42b64c944523e9f88e9fdd02a5539581ad71b389
kosu <kosumon27@gmail.com>
2022-12-05 23:59:52 +0900

    💄 style変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-default-export */
 /* eslint-disable react/destructuring-assignment */
-import '../styles/globals.css'
+import '../styles/tailwind.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 import { MantineProvider } from '@mantine/core'
