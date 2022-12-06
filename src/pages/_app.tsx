f6c7c4964909827bfd123185ac25e840f3f6a855
kosu <kosumon27@gmail.com>
2022-12-06 23:24:00 +0900

    ➕ useDapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,35 @@ import '../styles/tailwind.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 import { MantineProvider } from '@mantine/core'
+import { DAppProvider, Hardhat } from '@usedapp/core'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+const config = {
+  multicallAddresses: {
+    [Hardhat.chainId]: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  },
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        /** Put your mantine theme override here */
-        colorScheme: 'light',
-      }}
-    >
-      <Component {...pageProps} />
-    </MantineProvider>
+    <DAppProvider config={config}>
+      <Head>
+        <title>Toilet App</title>
+        <meta name="description" content="Toilet App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </DAppProvider>
   )
 }
 
